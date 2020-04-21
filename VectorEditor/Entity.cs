b1d6f8064b721905560ca1efcfc3960ddc713b4e
sim511777 <sim51177@gmail.com>
2020-04-21 09:45:20 +0900

    대문자 변경

--- VectorEditor/Entity.cs
+++ VectorEditor/Entity.cs
@@ -8,24 +8,24 @@ using System.Windows;
 
 namespace VectorEditor {
     public class HatchInfo {
-        public bool enabled { get; set; }
-        public double dist { get; set; }
-        public double angle { get; set; }
-        public double beamComp { get; set; }
-        public double startOffset { get; set; }
-        public double endOffset { get; set; }
+        public bool Enabled { get; set; }
+        public double Dist { get; set; }
+        public double Angle { get; set; }
+        public double BeamComp { get; set; }
+        public double StartOffset { get; set; }
+        public double EndOffset { get; set; }
     }
 
     public abstract class Entity {
         public string Name { get; set; }
-        public HatchInfo hatch1 { get; set; }
-        public HatchInfo hatch2 { get; set; }
+        public HatchInfo Hatch1 { get; set; }
+        public HatchInfo Hatch2 { get; set; }
         public abstract List<List<Vector>> GetBorders();
         public abstract List<List<Vector>> GetHatches();
     }
 
     public class Point : Entity {
-        public Vector ptCenter { get; set; }
+        public Vector PtCenter { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -37,8 +37,8 @@ namespace VectorEditor {
     }
 
     public class Line : Entity {
-        public Vector ptStart { get; set; }
-        public Vector ptEnd { get; set; }
+        public Vector PtStart { get; set; }
+        public Vector PtEnd { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -50,10 +50,10 @@ namespace VectorEditor {
     }
 
     public class Arc : Entity {
-        public Vector ptStart { get; set; }
-        public Vector ptEnd { get; set; }
-        public double bulge { get; set; }
-        public int resolition { get; set; }
+        public Vector PtStart { get; set; }
+        public Vector PtEnd { get; set; }
+        public double Bulge { get; set; }
+        public int Resolition { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -77,9 +77,9 @@ namespace VectorEditor {
     }
 
     public class Circle : Entity {
-        public Vector ptCenter { get; set; }
-        public double radius { get; set; }
-        public int resolition { get; set; }
+        public Vector PtCenter { get; set; }
+        public double Radius { get; set; }
+        public int Resolition { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -91,13 +91,13 @@ namespace VectorEditor {
     }
 
     public class Ellipse : Entity {
-        public Vector ptCenter { get; set; }
-        public double radiusX { get; set; }
-        public double radiusY { get; set; }
-        public double rotation { get; set; }
-        public double startAngle { get; set; }
-        public double sizeAngle { get; set; }
-        public int resolition { get; set; }
+        public Vector PtCenter { get; set; }
+        public double RadiusX { get; set; }
+        public double RadiusY { get; set; }
+        public double Rotation { get; set; }
+        public double StartAngle { get; set; }
+        public double SizeAngle { get; set; }
+        public int Resolition { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -109,14 +109,14 @@ namespace VectorEditor {
     }
 
     public class Spiral : Entity {
-        public Vector ptCenter { get; set; }
-        public double radiusX { get; set; }
-        public double radiusY { get; set; }
-        public double rotation { get; set; }
-        public double numberOfTurns { get; set; }
-        public double angularStep { get; set; }
-        public bool clockWise { get; set; }
-        public bool outerCircle { get; set; }
+        public Vector PtCenter { get; set; }
+        public double RadiusX { get; set; }
+        public double RadiusY { get; set; }
+        public double Rotation { get; set; }
+        public double NumberOfTurns { get; set; }
+        public double AngularStep { get; set; }
+        public bool ClockWise { get; set; }
+        public bool OuterCircle { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -128,10 +128,10 @@ namespace VectorEditor {
     }
 
     public class Rectangle : Entity {
-        public Vector ptCenter { get; set; }
-        public double width { get; set; }
-        public double height { get; set; }
-        public double rotation { get; set; }
+        public Vector PtCenter { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Rotation { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -143,10 +143,10 @@ namespace VectorEditor {
     }
 
     public class Text : Entity {
-        public Vector ptCenter { get; set; }
-        public string text { get; set; }
-        public Font font { get; set; }
-        public double rotation { get; set; }
+        public Vector PtCenter { get; set; }
+        public string Str { get; set; }
+        public Font Font { get; set; }
+        public double Rotation { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -164,12 +164,12 @@ namespace VectorEditor {
     }
 
     public class Barcode : Entity {
-        public Vector ptCenter { get; set; }
-        public double width { get; set; }
-        public double height { get; set; }
-        public double rotation { get; set; }
-        public BarcodeType codeType { get; set; }
-        public string codeText { get; set; }
+        public Vector PtCenter { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Rotation { get; set; }
+        public BarcodeType CodeType { get; set; }
+        public string CodeText { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
@@ -181,11 +181,11 @@ namespace VectorEditor {
     }
 
     public class Image : Entity {
-        public Vector ptCenter { get; set; }
-        public double width { get; set; }
-        public double height { get; set; }
-        public double rotation { get; set; }
-        public Bitmap bitmap { get; set; }
+        public Vector Center { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Rotation { get; set; }
+        public Bitmap Bitmap { get; set; }
 
         public override List<List<Vector>> GetBorders() {
             throw new NotImplementedException();
